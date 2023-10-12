018c65015c37cb0216cae0f035cff74b76fa71ed
Alexandre <alexandreaddres@gmail.com>
2023-10-12 00:03:33 -0300

    adding new users to database h2 with sucess 200

--- todolist/src/main/java/tech/alxinc/todolist/user/UserController.java
+++ todolist/src/main/java/tech/alxinc/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package tech.alxinc.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println("Created: " + userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel){
+        var userCreated = this.userRepository.save(userModel);
+
+        return userCreated;
     }
+
+
 }
