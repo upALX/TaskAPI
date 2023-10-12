645a4d649abcf05dd5ed22cbf1f1468b3e497aa8
Alexandre <alexandreaddres@gmail.com>
2023-10-11 21:10:41 -0300

    adding user model controller to project

---
+++ todolist/src/main/java/tech/alxinc/todolist/user/UserController.java
@@ -0,0 +1,17 @@
+package tech.alxinc.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/create")
+
+public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println("Created: " + userModel.name);
+    }
+}
