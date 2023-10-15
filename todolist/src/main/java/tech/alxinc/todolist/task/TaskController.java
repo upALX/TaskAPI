97a5a8aeca0e59cc4ff3230e238230cea908a9ad
Alexandre <alexandreaddres@gmail.com>
2023-10-15 04:48:00 -0300

    adding custom errors date on register new task

--- todolist/src/main/java/tech/alxinc/todolist/task/TaskController.java
+++ todolist/src/main/java/tech/alxinc/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package tech.alxinc.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +21,26 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
         System.out.println("Controller" + request.getAttribute("idUser"));
+
         var idUser = request.getAttribute("idUser");
         taskModel.setUserId((UUID) idUser);
-        var newTask = this.taskRepository.save(taskModel);
 
-        return newTask;
+        var CurrentDate = LocalDateTime.now();
+
+        if(CurrentDate.isAfter(taskModel.getStartAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The START date must be greater than the current date");
+        }else if(CurrentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The END date must be greater than the current date");
+        }else if (taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The START date must be greater than END date");
+        }else{
+            var newTask = this.taskRepository.save(taskModel);
+
+            return ResponseEntity.status(HttpStatus.OK).body(newTask);
+        }
     }
+
+    
 }
